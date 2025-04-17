2b12a561d5e3e5fac325a354115a94cfeb7760bd
Hitezverma12 <hitezverma12@yahoo.com>
2025-04-17 17:17:43 +1000

    tuplex.py

---
+++ tup.py
@@ -0,0 +1,19 @@
+tuplex = ("tuple",False,3.2,1)
+print(tuplex)
+
+tuplex = (4,6,2,8,3,2,1)
+print(tuplex)
+
+tuplex = tuplex + (9,2)
+print(tuplex)
+      
+
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+
+tuplex = (2,4,3,5,4,6,7,8,6,1)
+mcdonalds = tuplex[3:5]
+
+print(mcdonalds)
+
+mcdonalds =tuplex[:6]
